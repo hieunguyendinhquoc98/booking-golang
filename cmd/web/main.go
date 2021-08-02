4a17919a5a4de4f0049a86128073913d3d8b7c9e
Nguyen Dinh Quoc Hieu <you@example.com>
2021-08-02 11:07:19 +0700

    first-commit

---
+++ cmd/web/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"encoding/gob"
+	"fmt"
+	"github.com/alexedwards/scs/v2"
+	"github.com/hieunguyendinhquoc98/bookings/internal/config"
+	"github.com/hieunguyendinhquoc98/bookings/internal/handlers"
+	"github.com/hieunguyendinhquoc98/bookings/internal/models"
+	"github.com/hieunguyendinhquoc98/bookings/internal/render"
+	"log"
+	"net/http"
+	"time"
+)
+
+const portNumber = ":8080"
+
+var app config.AppConfig
+var session *scs.SessionManager
+
+func main() {
+	err := run()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(fmt.Sprintf("Staring application on port %s", portNumber))
+	srv := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run() error {
+	// what am I going to put in the session
+	gob.Register(models.Reservation{})
+
+	// change this to true when in production
+	app.InProduction = false
+
+	// set up the session
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+		return err
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplates(&app)
+	return nil
+}
