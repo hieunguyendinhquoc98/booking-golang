f24f973a70d52ab32308e2a26a4d1662f50cd73b
Nguyen Dinh Quoc Hieu <you@example.com>
2021-08-05 11:47:53 +0700

    deploying booking, connect SQL

--- internal/handlers/setup_test.go
+++ internal/handlers/setup_test.go
@@ -50,7 +50,7 @@ func getRoutes() http.Handler {
 	repo := NewRepo(&app)
 	NewHandlers(repo)
 
-	render.NewTemplates(&app)
+	render.NewRenderer(&app)
 
 	mux := chi.NewRouter()
 
---
+++ internal/repository/repository.go
@@ -0,0 +1,15 @@
+package repository
+
+import (
+	"github.com/hieunguyendinhquoc98/bookings/internal/models"
+	"time"
+)
+
+type DatabaseRepo interface {
+	AllUsers() bool
+	InsertReservation(res models.Reservation) (int, error)
+	InsertRoomRestriction(r models.RoomRestriction) error
+	SearchAvailabilityByDatesByRoomID(start, end time.Time, roomID int) (bool, error)
+	SearchAvailabilityForAllRooms(start, end time.Time) ([]models.Room, error)
+	GetRoomByID(roomID int) (models.Room, error)
+}
